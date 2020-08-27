4b4bf385f60fd6124fe36aaafe624fc9f497783a
serializedowen <wjh199455@gmail.com>
2020-08-27 18:05:12 +0800

    Updates

---
+++ precache-manifest.af13dcd30aee9936b294180a7eb6da3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8069bdf867d06eceb51b2d916a1e699",
+    "url": "/pornhub-styled-logo-generator/index.html"
+  },
+  {
+    "revision": "1f965efa10411ce10900",
+    "url": "/pornhub-styled-logo-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fef675c42ee0cfca210d",
+    "url": "/pornhub-styled-logo-generator/static/js/2.f18c6d82.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pornhub-styled-logo-generator/static/js/2.f18c6d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1f175a050418d8b1ad",
+    "url": "/pornhub-styled-logo-generator/static/js/3.dca37a2e.chunk.js"
+  },
+  {
+    "revision": "fc36a98eb489522bae86",
+    "url": "/pornhub-styled-logo-generator/static/js/4.04de468d.chunk.js"
+  },
+  {
+    "revision": "1f965efa10411ce10900",
+    "url": "/pornhub-styled-logo-generator/static/js/main.a8b61ced.chunk.js"
+  },
+  {
+    "revision": "0d5e54dc4fa686fc6c09",
+    "url": "/pornhub-styled-logo-generator/static/js/runtime-main.3497beed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59b43be03c35772705d194ca48fd9821.js"
+  "/pornhub-styled-logo-generator/precache-manifest.af13dcd30aee9936b294180a7eb6da3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pornhub-styled-logo-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
